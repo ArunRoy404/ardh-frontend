a06543504fa6a30beabd9d9b2101d2f8d2c831ff
ArunRoy404 <arunroy0065@gmail.com>
2026-06-14 16:33:31 +0600

    feat: implement sign-in page with react-hook-form, zod validation, and reusable UI components.

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,10 +1,11 @@
+import SignInPage from "@/pages/auth/SignInPage";
 import { createBrowserRouter } from "react-router";
 
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <div>Hello World</div>,
+    path: "/sign-in",
+    element: <SignInPage />,
   },
 ]);
 
