a1abfb1774c8dd510db4ba997d00d8176d47d0a7
ArunRoy404 <arunroy0065@gmail.com>
2026-06-14 21:42:32 +0600

    feat: create DashboardHeader component with sidebar toggle, notifications, user profile, and logout action

--- src/components/DashboardLayout/DashboardHeader.jsx
+++ src/components/DashboardLayout/DashboardHeader.jsx
@@ -2,6 +2,7 @@ import { SidebarTrigger } from "../ui/sidebar"
 import LogoutIcon from "../SvgIcons/LogoutIcon"
 import { NotificationsIcon } from "../SvgIcons/NotificationsIcon"
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar"
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "../ui/tooltip"
 
 const DashboardHeader = () => {
     return (
@@ -9,21 +10,31 @@ const DashboardHeader = () => {
             <div className="flex items-center gap-2">
                 <SidebarTrigger className="p-4!" />
             </div>
-            
+
             <div className="flex items-center gap-4">
                 <button className="relative cursor-pointer text-muted-foreground hover:text-foreground flex items-center justify-center">
                     <NotificationsIcon className="size-6" />
-                    <span className="absolute top-0 right-1 flex size-2 rounded-full bg-red-500"></span>
+                    <span className="absolute top-0 right-0 flex h-3.5 w-3.5 items-center justify-center rounded-full bg-red-500 text-[10px] font-bold text-white">3</span>
                 </button>
-                
-                <Avatar className="cursor-pointer size-8">
-                    <AvatarImage src="https://github.com/shadcn.png" alt="Profile" />
-                    <AvatarFallback>PR</AvatarFallback>
-                </Avatar>
-                
-                <button className="flex items-center justify-center text-muted-foreground hover:text-foreground cursor-pointer">
-                    <LogoutIcon className="size-6" />
-                </button>
+
+                <div className="flex items-center justify-center gap-2">
+                    <Avatar className="cursor-pointer size-8">
+                        <AvatarImage src="https://github.com/shadcn.png" alt="Profile" />
+                        <AvatarFallback>PR</AvatarFallback>
+                    </Avatar>
+                    <p className="text-xs text-dark-gray">John Doe</p>
+                </div>
+
+                <TooltipProvider>
+                    <Tooltip>
+                        <TooltipTrigger className="flex items-center justify-center text-red-500 hover:text-red-600 cursor-pointer">
+                            <LogoutIcon className="size-6" />
+                        </TooltipTrigger>
+                        <TooltipContent>
+                            <p>Logout</p>
+                        </TooltipContent>
+                    </Tooltip>
+                </TooltipProvider>
             </div>
         </header>
     )
