f4f882fdd6c749f7a0fbade03ca324cb7485efc2
ArunRoy404 <arunroy0065@gmail.com>
2026-06-16 23:39:15 +0600

    feat: add maintenance page with ticket list and management actions

--- src/pages/dashboard/MaintenancePage.jsx
+++ src/pages/dashboard/MaintenancePage.jsx
@@ -1,8 +1,40 @@
+import DashboardHeader from "@/components/DashboardLayout/DashbaordHeader";
+import MaintenanceList from "@/components/Maintenance/MaintenanceList/MaintenanceList";
+import { Button } from "@/components/ui/button";
+import { PlusCircle, Upload } from "lucide-react";
+import { useMemo } from "react";
+
 const MaintenancePage = () => {
+  // Maintenance Mock Data
+  const maintenanceData = useMemo(() => [
+    { id: 101, ticketId: "MNT-701", issue: "Leaking water pipe in kitchen", type: "Plumbing", floor: 1, tenant: "Ankit Varma", status: "Open", flat: "101" },
+    { id: 102, ticketId: "MNT-702", issue: "AC not cooling", type: "HVAC", floor: 3, tenant: "Preeti Singh", status: "Open", flat: "303" },
+  ], [])
+
+
   return (
-    <div>
-      <h1 className="text-2xl font-bold">Maintenance</h1>
-    </div>
+    <>
+      {/* Header Block */}
+      <DashboardHeader
+        title="Maintenance"
+        showBreadcrumb
+      >
+        <div className="flex items-center justify-end gap-2">
+          <Button variant="outline">
+            <Upload />
+            Bulk Upload
+          </Button>
+          <Button>
+            <PlusCircle className="w-4 h-4" />
+            Add New Maintenance
+          </Button>
+        </div>
+      </DashboardHeader>
+
+      <MaintenanceList
+        data={maintenanceData}
+      />
+    </>
   );
 };
 export default MaintenancePage;
